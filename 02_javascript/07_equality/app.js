9eefa37da47412e4b455847754eba3fa6da17a96
Christian Tisby <cduane82@hotmail.com>
2020-04-19 23:25:55 -0400

    adding equality

---
+++ 02_javascript/07_equality/app.js
@@ -0,0 +1,18 @@
+let number = 7
+let numberObj1 = {
+    num: 7
+}
+
+let numberObj2 = {
+    num: 7
+}
+
+ numberObj3 === numberObj3
+
+ let name = "Jesse"
+ let newName = name
+
+ newName = "Jesse Two"
+
+ console.log(name)
+ console.log(newName)
