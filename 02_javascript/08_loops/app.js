72a18b6eb4586cc7cd0f92eeda519dbe68c1e3aa
Christian Tisby <cduane82@hotmail.com>
2020-04-24 23:57:29 -0400

    adding completed js section

---
+++ 02_javascript/08_loops/app.js
@@ -0,0 +1,12 @@
+let nerds = ["Albert Einstein", "Alan Turing", "Nikola Tesla", "Isaac Newton", "Christian Tisby"]
+
+for (i = 0; i <= nerds.length; i++) {
+    console.log(nerds[i])
+}
+
+let number = 10 
+
+while(number) {
+    console.log("Do this")
+    number--
+}
